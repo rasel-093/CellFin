17bf32594baa88abca2fe8aa3ac5f9836e9b6284
niloy32073 <86624205+niloy32073@users.noreply.github.com>
2023-11-19 00:01:45 +0600

    Update SampleItem.kt

--- app/src/main/java/com/example/cellfin/model/SampleItem.kt
+++ app/src/main/java/com/example/cellfin/model/SampleItem.kt
@@ -90,106 +90,140 @@ val trxList = listOf(
         trxId = "8723092700007763",
         amount = "150",
         trxType = false
+    ),
+    TrxItem(
+        id = 11,
+        title = "CRM Cash IN",
+        details = "CRM Deposit - From Terminal IBCR5386",
+        date = "24/10/2023",
+        trxId = "8723092700007769",
+        amount = "15000",
+        trxType = true
+    ),
+    TrxItem(
+        id = 12,
+        title = "ATM Withdrawal CellFin",
+        details = "From 01618964627",
+        date = "23/10/2023",
+        trxId = "8723092700007745",
+        amount = "50000",
+        trxType = false
+    ),
+    TrxItem(
+        id = 13,
+        title = "Mobile Top Up",
+        details = "Mobile Recharge to 01618964627",
+        date = "23/10/2023",
+        trxId = "8723092700007763",
+        amount = "500",
+        trxType = false
+    ),
+    TrxItem(
+        id = 14,
+        title = "ATM Withdrawal CellFin",
+        details = "From 01618964627",
+        date = "23/10/2023",
+        trxId = "8723092700007709",
+        amount = "20000",
+        trxType = false
+    ),
+    TrxItem(
+        id = 15,
+        title = "CRM Cash IN",
+        details = "CRM Deposit - From Terminal IBCR5386",
+        date = "22/10/2023",
+        trxId = "8723092700007732",
+        amount = "50000",
+        trxType = true
+    ),
+    TrxItem(
+        id = 16,
+        title = "Inward NPSB",
+        details = "Npsb payment from THE CITY BANK LTD. acc 220256794001",
+        date = "21/10/2023",
+        trxId = "8723092700007787",
+        amount = "70000",
+        trxType = true
+    ),
+    TrxItem(
+        id =17,
+        title = "ATM Withdrawal CellFin",
+        details = "From 01618964627",
+        date = "21/10/2023",
+        trxId = "8723092700007709",
+        amount = "12000",
+        trxType = false
+    ),
+    TrxItem(
+        id = 18,
+        title = "CRM Cash IN",
+        details = "CRM Deposit - From Terminal IBCR5395",
+        date = "20/10/2023",
+        trxId = "8723092700007769",
+        amount = "20000",
+        trxType = true
+    ),
+    TrxItem(
+        id = 19,
+        title = "Inward NPSB",
+        details = "Npsb payment from THE CITY BANK LTD. acc 220256794001",
+        date = "19/10/2023",
+        trxId = "8723092700007632",
+        amount = "40000",
+        trxType = true
+    ),
+    TrxItem(
+        id = 20,
+        title = "Mobile Top Up",
+        details = "Mobile Recharge to 01618964627",
+        date = "19/10/2023",
+        trxId = "8723092700007763",
+        amount = "250",
+        trxType = false
+    ),
+    TrxItem(
+        id = 21,
+        title = "Inward NPSB",
+        details = "Npsb payment from THE CITY BANK LTD. acc 220256794001",
+        date = "19/10/2023",
+        trxId = "8723092700007787",
+        amount = "7000",
+        trxType = true
+    ),
+    TrxItem(
+        id =22,
+        title = "ATM Withdrawal CellFin",
+        details = "From 01618964627",
+        date = "19/10/2023",
+        trxId = "8723092700007709",
+        amount = "15000",
+        trxType = false
+    ),
+    TrxItem(
+        id = 23,
+        title = "CRM Cash IN",
+        details = "CRM Deposit - From Terminal IBCR5395",
+        date = "19/10/2023",
+        trxId = "8723092700007769",
+        amount = "20000",
+        trxType = true
+    ),
+    TrxItem(
+        id = 24,
+        title = "Inward NPSB",
+        details = "Npsb payment from THE CITY BANK LTD. acc 220256794001",
+        date = "19/10/2023",
+        trxId = "8723092700007632",
+        amount = "50000",
+        trxType = true
+    ),
+    TrxItem(
+        id = 25,
+        title = "Mobile Top Up",
+        details = "Mobile Recharge to 01618964627",
+        date = "19/10/2023",
+        trxId = "8723092700007763",
+        amount = "350",
+        trxType = false
     )
 )
-
-
-
-
-
-
-//var trxList = listOf(
-//    TrxItem(
-//        id = 1,
-//        title = "CRM Cash IN",
-//        details = "CRM Deposit - From Terminal IBCR5386",
-//        date = "18/11/2023",
-//        trxId = "8723092700007769",
-//        amount = "5000",
-//        trxType = true
-//    ),
-//    TrxItem(
-//        id = 2,
-//        title = "ATM Withdrawal CellFin",
-//        details = "From 01618964627",
-//        date = "17/11/2023",
-//        trxId = "8723092700007745",
-//        amount = "10000",
-//        trxType = false
-//    ),
-//    TrxItem(
-//        id = 3,
-//        title = "Mobile Top Up",
-//        details = "Mobile Recharge to 01618964627",
-//        date = "17/11/2023",
-//        trxId = "8723092700007763",
-//        amount = "100",
-//        trxType = false
-//    ),
-//    TrxItem(
-//        id = 4,
-//        title = "ATM Withdrawal CellFin",
-//        details = "From 01618964627",
-//        date = "15/11/2023",
-//        trxId = "8723092700007709",
-//        amount = "10000",
-//        trxType = false
-//    ),
-//    TrxItem(
-//        id = 5,
-//        title = "CRM Cash IN",
-//        details = "CRM Deposit - From Terminal IBCR5386",
-//        date = "14/11/2023",
-//        trxId = "8723092700007732",
-//        amount = "50000",
-//        trxType = true
-//    ),
-//    TrxItem(
-//        id = 6,
-//        title = "Inward NPSB",
-//        details = "Npsb payment from THE CITY BANK LTD. acc 220256794001",
-//        date = "14/11/2023",
-//        trxId = "8723092700007787",
-//        amount = "60000",
-//        trxType = true
-//    ),
-//
-//    TrxItem(
-//        id = 7,
-//        title = "ATM Withdrawal CellFin",
-//        details = "From 01618964627",
-//        date = "13/11/2023",
-//        trxId = "8723092700007709",
-//        amount = "15000",
-//        trxType = false
-//    ),
-//    TrxItem(
-//        id = 7,
-//        title = "CRM Cash IN",
-//        details = "CRM Deposit - From Terminal IBCR5395",
-//        date = "13/11/2023",
-//        trxId = "8723092700007769",
-//        amount = "2000",
-//        trxType = true
-//    ),
-//    TrxItem(
-//        id = 8,
-//        title = "Inward NPSB",
-//        details = "Npsb payment from THE CITY BANK LTD. acc 220256794001",
-//        date = "12/11/2023",
-//        trxId = "8723092700007632",
-//        amount = "40000",
-//        trxType = true
-//    ),
-//    TrxItem(
-//        id = 9,
-//        title = "Mobile Top Up",
-//        details = "Mobile Recharge to 01618964627",
-//        date = "17/11/2023",
-//        trxId = "8723092700007763",
-//        amount = "150",
-//        trxType = false
-//    ),
-//
-//
-//)
