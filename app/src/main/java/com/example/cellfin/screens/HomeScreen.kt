bebbd7ffa13e039e501615c2b4538af641d0691d
rasel <rasel.cse19.ruet@gmail.com>
2023-11-19 07:54:08 +0600

    Bottomsheet added(incomplete)

--- app/src/main/java/com/example/cellfin/screens/HomeScreen.kt
+++ app/src/main/java/com/example/cellfin/screens/HomeScreen.kt
@@ -12,14 +12,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,6 +38,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.cellfin.Components.Carousel
@@ -35,10 +46,12 @@ import com.example.cellfin.R
 import com.example.cellfin.StatementActivity
 import com.google.accompanist.pager.ExperimentalPagerApi
 
-@OptIn(ExperimentalPagerApi::class)
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController){
-    val context = LocalContext.current
+    val context = LocalContext.current // used in intent
+    val sheetState = rememberModalBottomSheetState()
+    var isSheetOpen by rememberSaveable { mutableStateOf(false) }
 
     Column (modifier = Modifier.fillMaxWidth()){
         Row(modifier = Modifier
@@ -95,7 +108,8 @@ fun HomeScreen(navController: NavController){
 
                     Image(painter = painterResource(id = R.drawable.group1), contentDescription = "One", modifier = Modifier
                         .fillMaxWidth(.95f)
-                        .shadow(5.dp, RoundedCornerShape(10.dp), clip = true), contentScale = ContentScale.FillWidth)
+                        .shadow(5.dp, RoundedCornerShape(10.dp), clip = true)
+                        .clickable { isSheetOpen = true }, contentScale = ContentScale.FillWidth)
                     Spacer(modifier = Modifier.height(20.dp))
 
                     Carousel()
@@ -110,13 +124,65 @@ fun HomeScreen(navController: NavController){
                     modifier = Modifier
                         .fillMaxWidth()
                         .clickable {
-                              val intent = Intent(context,StatementActivity::class.java)
-                              context.startActivity(intent)
+                            val intent = Intent(context, StatementActivity::class.java)
+                            context.startActivity(intent)
                         },
                     contentScale = ContentScale.FillWidth)
             }
         }
+    }
 
+    //Bottom sheet
+    var title by rememberSaveable { mutableStateOf("") }
+    var details by rememberSaveable { mutableStateOf("") }
+    var date by rememberSaveable { mutableStateOf("") }
+    var trxId by rememberSaveable { mutableStateOf("") }
+    var amount by rememberSaveable { mutableStateOf("") }
+    var type by rememberSaveable { mutableStateOf("") }
 
+    if(isSheetOpen){
+        ModalBottomSheet(
+            sheetState = sheetState,
+            onDismissRequest = { isSheetOpen = false }
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth().padding(bottom = 20.dp)
+            ) {
+                OutlinedTextField(
+                    value = title,
+                    onValueChange = {title = it},
+                    label = { Text(text = "Title")}
+                )
+                OutlinedTextField(
+                    value = details,
+                    onValueChange = {details = it},
+                    label = {Text(text = "Details")}
+
+                )
+                OutlinedTextField(
+                    value = date,
+                    onValueChange = {date = it},
+                    label = {Text(text = "Date")}
+                )
+                OutlinedTextField(
+                    value = trxId,
+                    onValueChange = {trxId = it},
+                    label = {Text(text = "Transaction Id")}
+                )
+                OutlinedTextField(
+                    value = amount,
+                    onValueChange = {amount = it},
+                    label = {Text(text = "amount")},
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    )
+                )
+                //Add radio button here
+                //For cash in -> type = true.toString()
+                //For cashout -> type = false.toString()
+            }
+        }
     }
 }
